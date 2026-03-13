507a65d78a2f9416f0e3e6fb02dbf40728b3cc19
JAVB2077 <joe.tecnm@gmail.com>
2026-03-12 23:42:28 -0600

    actualizacion de datos

--- src/db/index.ts
+++ src/db/index.ts
@@ -3,7 +3,13 @@ import { drizzle } from 'drizzle-orm/neon-http'
 import * as schema from './schema'
 
 if (!process.env.DATABASE_URL) {
-  throw new Error('DATABASE_URL no definida en .env')
+  throw new Error('DATABASE_URL no definida en .env')DATABASE_URL="postgresql://neondb_owner:npg_dr4tiQq6XsWG@ep-polished-queen-akmsry7t.c-3.us-west-2.aws.neon.tech/neondb?sslmode=require&channel_binding=require"
+
+JWT_SECRET="grand-elite-secret-2025"
+
+PORT=3000
+NODE_ENV=development
+FRONTEND_URL=http://localhost:4321
 }
 
 const sql = neon(process.env.DATABASE_URL)
